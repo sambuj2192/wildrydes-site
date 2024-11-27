6c665f282806ffcd12bfeef2ba99bb60c81fdbfa
sambuj2192 <162291596+sambuj2192@users.noreply.github.com>
2024-11-27 17:57:12 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_jDBo2lT2m', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1n4deqg3l2cq27f5svm4dksq3n', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
